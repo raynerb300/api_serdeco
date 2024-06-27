4ebb40250c16ee85f2726bbfbd07eb302e107ac7
DanielJoseGonzalezGonzalez <danieljosegonzalezg@gmail.com>
2024-06-27 14:53:50 -0400

    aqui hice que el txt se elimine tras la creacion de ZIP y tambien arregle un error en cuanto a los archivos que tomaba el ZIP

--- src/utils/CompressFile.js
+++ src/utils/CompressFile.js
@@ -5,8 +5,10 @@ export function CompressFile(nombreArchivo){
     let ubicacionArchivo = "./reports/"
     let output = fs.createWriteStream (ubicacionArchivo + nombreArchivo + ".zip");
     let archive = archiver ("zip");
-
     archive.pipe (output);
-    archive.directory (ubicacionArchivo, "Archivos")
-    archive.finalize()
+
+    archive.append(fs.createReadStream("./reports/documento.txt"), {name: "documentoZip.txt"});
+    archive.finalize().then(() => {
+        fs.unlinkSync("./reports/documento.txt")
+    });
 }
