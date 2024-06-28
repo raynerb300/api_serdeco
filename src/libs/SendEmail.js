938b6ab701218bd3bb579ab0486014e17cc4df3f
DiegoRNoguera <diegoirnoguera@gmail.com>
2024-06-28 15:12:36 -0400

    Aqui hice el sistema de envio de correos.

--- src/libs/SendEmail.js
+++ src/libs/SendEmail.js
@@ -1,10 +1,29 @@
 import { generateFile } from "../utils/GenerateFile.js";
 import { CompressFile } from "../utils/CompressFile.js";
 import { ReportFAGA } from "./ReportFAGA.js";
+import { Resend } from 'resend';
 
 export async function SendFAGA(nombreDelComprimido) {
 const data = await ReportFAGA()
 generateFile(data)
 CompressFile(nombreDelComprimido)
 console.log(nombreDelComprimido+".zip");
-} 
+
+}
+
+const resend = new Resend('re_LWqp5V9A_BnbapR1dfFEhV5Zar1oC76Li');
+
+(async function() {
+  const { data, error } = await resend.emails.send({
+    from: 'Acme<onboarding@resend.dev>',
+    to: ['diegoirnoguera@gmail.com'],
+    subject: 'Hello World',
+    html: '<strong>it works!</strong>'
+  });
+
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(data);
+})();
